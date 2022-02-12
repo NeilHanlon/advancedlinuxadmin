664aa103c21c455f890a2b937e93491159579945
Neil Hanlon <neil@rockylinux.org>
2022-02-12 15:58:46 -0500

    tf push

--- terraform/vultr/servers.tf
+++ terraform/vultr/servers.tf
@@ -55,24 +55,129 @@ resource "vultr_block_storage" "cinder" {
 
 
 
-resource "vultr_instance" "aio1" {
-  plan                = "vc2-4c-8gb"
-  region              = "ewr"
-  os_id               = "448"
-  label               = "aio1"
-  tag                 = "advancedlsa aio" #NO infra tag
-  hostname            = "ala-aio1"
-  enable_ipv6         = false
-  backups             = "disabled"
-  ddos_protection     = false
-  activation_email    = false
-  script_id           = "0eeabbfb-2d2f-4797-a85a-82d3e1f235bd"
-  ssh_key_ids         = [vultr_ssh_key.terraform.id]
-  private_network_ids = [vultr_private_network.mgmt2.id]
-}
-resource "vultr_block_storage" "cinder-aio" {
-  size_gb              = 100
-  region               = "ewr"
-  label                = "ala-storage-cinder-aio"
-  attached_to_instance = vultr_instance.aio1.id
-}
+#resource "vultr_instance" "aio1" {
+#  plan                = "vc2-4c-8gb"
+#  region              = "ewr"
+#  os_id               = "448"
+#  label               = "aio1"
+#  tag                 = "advancedlsa aio" #NO infra tag
+#  hostname            = "ala-aio1"
+#  enable_ipv6         = false
+#  backups             = "disabled"
+#  ddos_protection     = false
+#  activation_email    = false
+#  script_id           = "0eeabbfb-2d2f-4797-a85a-82d3e1f235bd"
+#  ssh_key_ids         = [vultr_ssh_key.terraform.id]
+#  private_network_ids = [vultr_private_network.mgmt2.id]
+#}
+#resource "vultr_block_storage" "cinder-aio" {
+#  size_gb              = 100
+#  region               = "ewr"
+#  label                = "ala-storage-cinder-aio"
+#  attached_to_instance = vultr_instance.aio1.id
+#}
+#resource "vultr_instance" "aio2" {
+#  plan                = "vc2-4c-8gb"
+#  region              = "ewr"
+#  os_id               = "448"
+#  label               = "aio2"
+#  tag                 = "advancedlsa aio" #NO infra tag
+#  hostname            = "ala-aio2"
+#  enable_ipv6         = false
+#  backups             = "disabled"
+#  ddos_protection     = false
+#  activation_email    = false
+#  script_id           = "0eeabbfb-2d2f-4797-a85a-82d3e1f235bd"
+#  ssh_key_ids         = [vultr_ssh_key.terraform.id]
+#  private_network_ids = [vultr_private_network.mgmt2.id]
+#}
+#resource "vultr_block_storage" "cinder-aio2" {
+#  size_gb              = 100
+#  region               = "ewr"
+#  label                = "ala-storage-cinder-aio"
+#  attached_to_instance = vultr_instance.aio2.id
+#}
+#resource "vultr_instance" "aio3" {
+#  plan                = "vc2-4c-8gb"
+#  region              = "ewr"
+#  os_id               = "448"
+#  label               = "aio3"
+#  tag                 = "advancedlsa aio" #NO infra tag
+#  hostname            = "ala-aio3"
+#  enable_ipv6         = false
+#  backups             = "disabled"
+#  ddos_protection     = false
+#  activation_email    = false
+#  script_id           = "0eeabbfb-2d2f-4797-a85a-82d3e1f235bd"
+#  ssh_key_ids         = [vultr_ssh_key.terraform.id]
+#  private_network_ids = [vultr_private_network.mgmt2.id]
+#}
+#resource "vultr_block_storage" "cinder-aio3" {
+#  size_gb              = 100
+#  region               = "ewr"
+#  label                = "ala-storage-cinder-aio"
+#  attached_to_instance = vultr_instance.aio3.id
+#}
+#resource "vultr_instance" "aio4" {
+#  plan                = "vc2-4c-8gb"
+#  region              = "ewr"
+#  os_id               = "448"
+#  label               = "aio4"
+#  tag                 = "advancedlsa aio" #NO infra tag
+#  hostname            = "ala-aio4"
+#  enable_ipv6         = false
+#  backups             = "disabled"
+#  ddos_protection     = false
+#  activation_email    = false
+#  script_id           = "0eeabbfb-2d2f-4797-a85a-82d3e1f235bd"
+#  ssh_key_ids         = [vultr_ssh_key.terraform.id]
+#  private_network_ids = [vultr_private_network.mgmt2.id]
+#}
+#resource "vultr_block_storage" "cinder-aio4" {
+#  size_gb              = 100
+#  region               = "ewr"
+#  label                = "ala-storage-cinder-aio"
+#  attached_to_instance = vultr_instance.aio4.id
+#}
+#resource "vultr_instance" "aio5" {
+#  plan                = "vc2-16c-64gb"
+#  region              = "ewr"
+#  os_id               = "448"
+#  label               = "aio5"
+#  tag                 = "advancedlsa aio" #NO infra tag
+#  hostname            = "ala-aio5"
+#  enable_ipv6         = false
+#  backups             = "disabled"
+#  ddos_protection     = false
+#  activation_email    = false
+#  script_id           = "0eeabbfb-2d2f-4797-a85a-82d3e1f235bd"
+#  ssh_key_ids         = [vultr_ssh_key.terraform.id]
+#  private_network_ids = [vultr_private_network.mgmt2.id]
+#}
+#resource "vultr_block_storage" "cinder-aio5" {
+#  size_gb              = 100
+#  region               = "ewr"
+#  label                = "ala-storage-cinder-aio"
+#  attached_to_instance = vultr_instance.aio5.id
+#}
+#resource "vultr_instance" "aio6" {
+#  plan                = "vc2-16c-64gb"
+#  region              = "ewr"
+#  os_id               = "448"
+#  label               = "aio6"
+#  tag                 = "advancedlsa aio" #NO infra tag
+#  hostname            = "ala-aio6"
+#  enable_ipv6         = false
+#  backups             = "disabled"
+#  ddos_protection     = false
+#  activation_email    = false
+#  script_id           = "0eeabbfb-2d2f-4797-a85a-82d3e1f235bd"
+#  ssh_key_ids         = [vultr_ssh_key.terraform.id]
+#  private_network_ids = [vultr_private_network.mgmt2.id]
+#}
+#resource "vultr_block_storage" "cinder-aio6" {
+#  size_gb              = 100
+#  region               = "ewr"
+#  label                = "ala-storage-cinder-aio"
+#  attached_to_instance = vultr_instance.aio6.id
+#}
